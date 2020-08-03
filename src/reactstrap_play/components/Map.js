7b2e085e30001a1043165b3e37fa1ef45004d3fe
unknown <ya3ya63@gmail.com>
2020-08-04 01:28:40 +0430

    changed-to-mapboxgl-for-better-performance-and-docs

--- src/reactstrap_play/components/Map.js
+++ src/reactstrap_play/components/Map.js
@@ -1,67 +1,60 @@
 import React, { Component } from "react";
-import ReactMapGL, {
-  setRTLTextPlugin,
-  NavigationControl,
-  Marker
-} from "react-map-gl";
-import Pin from "./Pin.js";
-setRTLTextPlugin(
+import MapboxGL from "mapbox-gl";
+const MAPBOXTOKEN =
+  "pk.eyJ1IjoieWEzeWE2IiwiYSI6ImNrZGVoYnYzMjBtYXkyd25ybnQ4d3RsNjkifQ.AJLX8R_aReMw7phl5OcfRQ";
+
+/*import Pin from "./Pin.js";*/
+/*setRTLTextPlugin(
   "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js",
   null,
   true
-);
+);*/
 export default class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewport: {
         width: "fit",
-        // latitude: 32.71381328503626,
-        // longitude: 51.66317365793152,
-        latitude: 53.52666097368566,
-        longitude: -113.52188775920357,
+        center: [-113.52188775920357, 53.52666097368566],
         zoom: 10,
         height: 500
-      }
-      /*coords: [
-        {
-          location: [latitude: 32.71091328503626,
-          longitude: 51.65657365793152],
-          title: "Home"
-        },
-        {
-          location: [latitude: 32.71281328503626,
-          longitude: 51.65917365793152],
-          title: "Park"
-        }
-      ]*/
+      },
+      map: null,
+      markersAdded: false
     };
   }
-  render() {
-    const { viewport } = this.state;
+  componentDidMount() {
+    MapboxGL.accessToken = MAPBOXTOKEN;
+    this.initializeMap();
+  }
+  initializeMap() {
+    let map = new MapboxGL.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      ...this.state.viewport
+    });
+    let nav = new MapboxGL.NavigationControl();
+    map.addControl(nav, "bottom-right");
+    this.setState({ map });
+  }
+  addMarkers() {
+    const { map } = this.state;
     const { data: coords } = this.props;
+    coords.forEach(coord => {
+      new MapboxGL.Marker()
+        .setLngLat([coord.location.longitude, coord.location.latitude])
+        .addTo(map);
+    });
+    this.setState({ markersAdded: true });
+  }
+  render() {
+    const { data: coords } = this.props;
+    const { map, markersAdded } = this.state;
+    if (map && !markersAdded && coords) this.addMarkers();
     return (
-      <ReactMapGL
-        mapboxApiAccessToken="pk.eyJ1IjoieWEzeWE2IiwiYSI6ImNrZGVoYnYzMjBtYXkyd25ybnQ4d3RsNjkifQ.AJLX8R_aReMw7phl5OcfRQ"
-        {...viewport}
-        onViewportChange={nextViewport =>
-          this.setState({ viewport: { ...nextViewport, width: "fit" } })
-        }
-        mapStyle="mapbox://styles/mapbox/outdoors-v11"
-      >
-        {(coords || []).map((coord, index) => (
-          <Marker
-            latitude={Number(coord.location.latitude)}
-            longitude={Number(coord.location.longitude)}
-          >
-            <Pin data={{ text: coord.closure }} markertitle={index} />
-          </Marker>
-        ))}
-
-        <div style={{ position: "absolute", right: 0, bottom: 0 }}>
-          <NavigationControl />
-        </div>
-      </ReactMapGL>
+      <div id="map" style={{ height: "490px" }}>
+        {}
+      </div>
     );
   }
 }
